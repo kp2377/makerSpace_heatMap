fdf8f54eda0d70bfa622da866132e4c9618baa39
kartikey purohit <kp2377@nyu.edu>
2019-07-18 23:09:44 -0400

    Initial commit

---
+++ personal/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me.','kp2377@nyu.edu']})
