9be0cf9da1ac62c3d423e5ecbef6384cabe1702a
kartikey purohit <kp2377@nyu.edu>
2019-07-21 00:14:52 -0400

    First Commit
    debug = False in settings.py

--- makerSpace_heatMap/settings.py
+++ makerSpace_heatMap/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hy)gc!p!=u9!cnp75vi8+7=s3z_l(e)5gl@uzp0)_p%^wyg$*h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
